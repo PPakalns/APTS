31d39301fc59099612ec2ca4b3dc3b5776e472c1
PPakalns <peterispakalns@gmail.com>
2017-01-13 23:51:49 +0200

    fix: additional changes to last commit

--- app/Http/Controllers/ProblemController.js
+++ app/Http/Controllers/ProblemController.js
@@ -4,6 +4,7 @@ const Problem = use('App/Model/Problem')
 const Helpers = use('Helpers')
 const Validator = use('Validator')
 var uuid = require('node-uuid')
+var yauzl = require("yauzl");
 
 class ProblemController {
 
@@ -89,8 +90,15 @@ class ProblemController {
     res.route('problem/show', {id: problemData.id})
   }
 
+  * test_edit(req, res) {
+    const id = req.param('id')
+    const problem = yield Problem.findOrFail(id)
+    yield res.sendView('problem/test/edit',{problem: problem.toJSON()})
+  }
 
-  * upload_tests(req, res) {
+  * test_edit_save(req, res) {
+    const data = req.only("id")
+
     // getting file instance
     const test_file = req.file('test_file', {
         maxSize: '100mb',
@@ -103,7 +111,7 @@ class ProblemController {
         .withAll()
         .andWith({'errors': [{message:"Norādiet pareizu testu failu."}]})
         .flash()
-      res.route('problem/create')
+      res.route('problem/test/edit', {id: data.id})
       return
     }
 
@@ -111,6 +119,7 @@ class ProblemController {
     const new_test_filename = uuid.v4();
 
     yield test_file.move(storagePath, new_test_filename)
+
     if (!test_file.moved()) {
       // Testa faila augšupielādes problēmas
       yield req
@@ -118,9 +127,66 @@ class ProblemController {
         .andWith({'errors': [{message:"Testu fails neatbilst ierobežojumiem. (Ierobežojumi: Līdz 100mb liels zip arhīvs)"}]})
         .flash()
       console.log(test_file.errors())
-      res.route('problem/create')
+      res.route('problem/test/edit', {id: data.id})
       return
     }
+    yield req
+      .withAll()
+      .andWith({'successes': [{message:"Augšupielāde izdevās"}]})
+      .flash()
+
+    var getZipFile = function (path_to_zip) {
+      return new Promise(function(resolve, reject){
+        yauzl.open(path_to_zip, {lazyEntries: true}, function(err, zipfile) {
+          if (err) reject(err);
+          resolve(zipfile);
+        })
+      })
+    }
+
+    var getZipFileNamePromise = function(zipfile){
+      return new Promise(function(resolve, reject){
+
+        var callbackOnEnd = function(){
+          removeListeners();
+          resolve(null)
+        }
+
+        var callbackOnEntry = function(entry){
+          if (/\/$/.test(entry.fileName)) {
+            // Skip directory
+            zipfile.readEntry()
+          } else {
+            removeListeners()
+            resolve(entry)
+          }
+        }
+        function removeListeners()
+        {
+          zipfile.removeListener("entry", callbackOnEntry)
+          zipfile.removeListener("end", callbackOnEnd)
+        }
+
+        zipfile.readEntry();
+
+        zipfile.on("entry", callbackOnEntry)
+        zipfile.once("end", callbackOnEnd)
+      })
+    }
+
+    var zipfile = yield getZipFile(test_file.uploadPath())
+    console.log(zipfile)
+
+    while ( true )
+    {
+      var filename = yield getZipFileNamePromise(zipfile);
+      if (!filename)
+        break;
+      console.log(filename.fileName)
+    }
+
+    res.route('problem/test/edit', {id: data.id})
+    return
   }
 }
 
--- app/Http/routes.js
+++ app/Http/routes.js
@@ -57,4 +57,7 @@ Route.group('problem', function(){
   Route.post('/create', 'ProblemController.create_save').as('problem/create_save')
   Route.get('/edit/:id', 'ProblemController.edit').as('problem/edit')
   Route.post('/edit/:id', 'ProblemController.edit_save').as('problem/edit_save')
+
+  Route.get('/test/edit/:id', 'ProblemController.test_edit').as('problem/test/edit')
+  Route.post('/test/edit/:id', 'ProblemController.test_edit_save').as('problem/test/edit_save')
 }).prefix('/problem')
