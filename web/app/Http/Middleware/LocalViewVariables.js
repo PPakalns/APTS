e7bb320d3b1bb964e82c81b2cb101fc9b3a8e862
PPakalns <peterispakalns@gmail.com>
2017-03-28 20:32:35 +0300

    fix: Server time and testing system status

--- web/app/Http/Middleware/LocalViewVariables.js
+++ web/app/Http/Middleware/LocalViewVariables.js
@@ -5,6 +5,7 @@ class LocalViewVariables {
     * handle (req, res, next) {
 
         res.viewInstance.global('server_time_offset', (new Date()).getTimezoneOffset())
+        res.viewInstance.global('server_time', (new Date()).toISOString())
 
         yield next
     }
