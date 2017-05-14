967219345e585c3bd213b1ac50a532a44acd7e22
PPakalns <peterispakalns@gmail.com>
2017-05-14 19:00:51 +0300

    fix: mariasql wrapper takes db not database
    see https://github.com/tgriesser/bookshelf/issues/415

--- web/config/database.js
+++ web/config/database.js
@@ -91,7 +91,7 @@ module.exports = {
             host: Env.get('DB_HOST', '127.0.0.1'),
             user: Env.get('DB_USER', 'root'),
             password: Env.get('DB_PASSWORD', ''),
-            database: Env.get('DB_DATABASE', 'adonis'),
+            db: Env.get('DB_DATABASE', 'adonis'),
             port: Env.get('DB_PORT', 3306)
         }
     }
